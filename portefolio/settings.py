4a068cde46264bd57da92ae94eed2f1377b3cc8d
RodrigueIragi <rodrigueiragi1@gmail>
2022-08-27 19:00:36 +0200

    Add heroku subdomain to ALLOWED_HOSTS

--- portefolio/settings.py
+++ portefolio/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-lpirfjzer&yqu-kx#w==13zq(vfgm1p0l0x6(bm4qielff6%3z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tech-hexa.heroku.com']
 
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 # Application definition
