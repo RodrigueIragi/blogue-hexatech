c531d73d17809a49bdfcf95fe32f43cecc3ccc50
RodrigueIragi <rodrigueiragi1@gmail>
2022-08-30 00:29:19 +0200

    Add requirements for heroku

--- portefolio/settings.py
+++ portefolio/settings.py
@@ -54,6 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'portefolio.urls'
@@ -138,5 +139,7 @@ STATIC_ROOT = BASE_DIR / os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/')
 ]
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 LOGIN_URL = "login"
