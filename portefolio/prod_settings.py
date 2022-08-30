b999803572ff5a36769bbf292e9a420ddbffabaa
RodrigueIragi <rodrigueiragi1@gmail>
2022-08-30 02:34:50 +0200

    Setup whitenoise for staticfiles delevery

--- portefolio/prod_settings.py
+++ portefolio/prod_settings.py
@@ -12,3 +12,7 @@ TEMPLATE_DEBUG = False
 SECRET_KEY = 's_k*9nf9n2lygyz1@8nhgor-d(1m(q6ls2bx5(sqr$m&q-=xq'
 
 ALLOWED_HOSTS = ['blogue-hexagone-tech.herokuapp.com']
+
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
+MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
