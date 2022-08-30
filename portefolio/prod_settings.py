8aefdf090ffd609df6316b152cb2e5c977556ff9
RodrigueIragi <rodrigueiragi1@gmail>
2022-08-30 16:38:36 +0200

    Configure production

--- portefolio/prod_settings.py
+++ portefolio/prod_settings.py
@@ -9,10 +9,47 @@ DEBUG = False
 TEMPLATE_DEBUG = False
 
 
-SECRET_KEY = 's_k*9nf9n2lygyz1@8nhgor-d(1m(q6ls2bx5(sqr$m&q-=xq'
-
 ALLOWED_HOSTS = ['blogue-hexagone-tech.herokuapp.com']
-
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MIDDLEWARE += ['whitenoise.middleware.WhiteNoiseMiddleware']
+SECRET_KEY = 's_k*9nf9n2lygyz1@8nhgor-d(1m(q6ls2bx5(sqr$m&q-=xq'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': ('%(asctime)s [%(process)d] [%(levelname)s] '
+                       'pathname=%(pathname)s lineno=%(lineno)s '
+                       'funcname=%(funcName)s %(message)s'),
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        }
+    },
+    'handlers': {
+        'null': {
+            'level': 'DEBUG',
+            'class': 'logging.NullHandler',
+        },
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'verbose'
+        }
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        },
+    }
+}
