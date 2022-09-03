d3b46cd6c372666e30f6a7dabcc17025e42c5349
RodrigueIragi <rodrigueiragi1@gmail>
2022-09-03 19:10:58 +0200

    fix some errors

--- portefolio/prod_settings.py
+++ portefolio/prod_settings.py
@@ -6,7 +6,5 @@ TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = ['blogue-tech-hexa.herokuapp.com']
 
-INSTALLED_APPS = ['whitenoise.runserver_nostatic',]
-
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
