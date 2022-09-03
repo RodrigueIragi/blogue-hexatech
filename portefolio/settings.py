1a4ed4d895261ce88cbb70d706334ec308339f44
RodrigueIragi <rodrigueiragi1@gmail>
2022-09-04 01:50:39 +0200

    Configure settings and release ALLOWED_HOSTS

--- portefolio/settings.py
+++ portefolio/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = 'django-insecure-lpirfjzer&yqu-kx#w==13zq(vfgm1p0l0x6(bm4qielff6%3z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['blogue-tech-hexa.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['blogue-hexagone-tech.herokuapp.com', '127.0.0.1']
 
 CKEDITOR_UPLOAD_PATH = 'uploads/'
 # Application definition
